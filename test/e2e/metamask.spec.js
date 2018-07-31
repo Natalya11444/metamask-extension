6548cb6cc571cf7c0842fddeb7c73dfa0064537a
Natalia <natalia.gulko@yahoo.com>
2018-07-31 00:43:33 -0700

    added parentheses to getting element in metamask.spec (test-e2e-firefox)

--- test/e2e/metamask.spec.js
+++ test/e2e/metamask.spec.js
@@ -124,7 +124,7 @@ describe('Metamask popup page', function () {
 
     it('shows value was created and seed phrase', async () => {
       await delay(300)
-      const seedPhrase = await driver.findElement(By.css('.twelve-word-phrase')).getText()
+      const seedPhrase = (await driver.findElement(By.css('.twelve-word-phrase'))).getText()
       assert.equal(seedPhrase.split(' ').length, 12)
       const continueAfterSeedPhrase = await driver.findElement(By.css('#app-content > div > div.app-primary.from-right > div > button:nth-child(4)'))
       assert.equal(await continueAfterSeedPhrase.getText(), `I'VE COPIED IT SOMEWHERE SAFE`)
